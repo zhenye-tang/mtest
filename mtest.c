11ae4c9c68b735bfe70151f2aba46d1f3daa818d
TangZhenye <361869840@qq.com>
2024-01-31 15:45:29 +0800

    update

--- mtest.c
+++ mtest.c
@@ -1,9 +1,10 @@
-#include "mtest.h"
+ï»¿#include "mtest.h"
 #include <string.h>
 #include <stdio.h>
 #include <finsh.h>
+#include "rtthread.h"
 
-
+#if defined(_MSC_VER)
 #pragma section("Mtest$a", read)
 const char __mtest_begin_name[] = "__start";
 const char __mtest_begin_desc[] = "begin of mtest";
@@ -23,18 +24,48 @@ __declspec(allocate("Mtest$z")) const struct uint_test __mtest_end =
     __mtest_end_desc,
     NULL
 };
+#elif defined (__GNUC__) || defined(__TI_COMPILER_VERSION__) || defined(__TASKING__)
+/* GNU GCC Compiler and TI CCS */
+extern struct uint_test __mtest_begin;
+extern struct uint_test __mtest_end;
+#endif
 
 TEST(haha, gaga) {
     EXPECT_STREQ("a", "b");
     EXPECT_STRCASEEQ("b", "B");
     EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 2);
+    EXPECT_EQ(1, 1);
     EXPECT_NE(1, 2);
+    EXPECT_END();
+}
+
+TEST(test, kaka) {
+    EXPECT_STREQ("a", "b");
+    EXPECT_STRCASEEQ("b", "B");
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(1, 1);
+    EXPECT_NE(1, 2);
+    EXPECT_END();
+}
+
+TEST(test, qqq) {
+    EXPECT_STREQ("a", "b");
+    EXPECT_STRCASEEQ("b", "B");
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(1, 1);
+    EXPECT_NE(1, 2);
+    EXPECT_END();
 }
 
 void mtest_run(int argc, char* argv)
 {
-    /* ±éÀú¶ÎÄÚ²âÊÔµ¥Ôª */
-
+    const struct uint_test* begin = (const struct uint_test*)&__mtest_begin + 1;
+    while (begin != &__mtest_end)
+    {
+        rt_kprintf("name = %s desc = %s\n", begin->name, begin->desc);
+        begin->test_entry(begin->name, begin->desc);
+        begin++;
+    }
 
 }
+MSH_CMD_EXPORT(mtest_run, mtest_run);
--- mtest.h
+++ mtest.h
@@ -6,7 +6,7 @@ struct uint_test
 {
     const char* name;
     const char* desc;
-    void (*test_entry)(void);
+    int (*test_entry)(const char *_name, const char *_desc);
 };
 
 #pragma section("Mtest$f",read)
@@ -17,50 +17,41 @@ struct uint_test
         MTEST_PRINTF(__VA_ARGS__); MTEST_PRINTF("\033[0m\n");
 
 #define MTEST_PRINT_ERROR(...)     MTEST_PRINTF("\033[31m"); \
+        MTEST_PRINTF("[%s] %s:%d", _name, __FILE__, __LINE__); \
         MTEST_PRINTF(__VA_ARGS__); MTEST_PRINTF("\033[0m\n");
 
 #define TO_STRINGI(x) #x
 #define STRING(s) TO_STRINGI(s)
 
 #define TEST(name, desc) \
-    static void name##desc##_entry(void);\
+    static int _##name##desc##_entry(const char *_name, const char *_desc);\
     __declspec(allocate("Mtest$f")) \
-    const struct uint_test _mtest##name##desc__ =             \
-        {STRING(name), STRING(desc), name##desc##_entry};   \
-    static void name##desc##_entry(void)
+    const struct uint_test _mtest_##name##_##desc##_ =             \
+        {STRING(name), STRING(desc), _##name##desc##_entry};   \
+    static int _##name##desc##_entry(const char *_name, const char *_desc)
+
 #pragma comment(linker, "/merge:Mtest=mytext") \
+
+#define MTEST_COMPARE(ne) \
+    if (!ne) {                           \
+        MTEST_PRINT_ERROR(" Failure"); \
+    }\
 
 #define EXPECT_STR_COMPARE(s1, s2, ne, cmp) \
     do {   \
         if (!cmp(s1, s2)) {                     \
-            if (ne) {                           \
-                MTEST_PRINT_OK("success!"); \
-            } else { \
-                MTEST_PRINT_ERROR("error!"); \
-            } \
+            MTEST_COMPARE(ne) \
         } else { \
-            if (!ne) { \
-                MTEST_PRINT_OK("success!"); \
-            } else { \
-                MTEST_PRINT_ERROR("error!"); \
-            } \
+            MTEST_COMPARE(!ne) \
         } \
     }while(0)
 
 #define EXPECT_INTERGER_COMPARE(s1, s2, ne) \
     do{ \
         if(s1 == s2) { \
-            if (ne) { \
-                    MTEST_PRINT_OK("success!"); \
-            } else { \
-                    MTEST_PRINT_ERROR("error!"); \
-            }\
+            MTEST_COMPARE(ne) \
         } else { \
-            if (!ne) { \
-                    MTEST_PRINT_OK("success!"); \
-            } else { \
-                    MTEST_PRINT_ERROR("error!"); \
-            }\
+            MTEST_COMPARE(!ne) \
         } \
     }while(0) \
 
@@ -89,5 +80,8 @@ struct uint_test
 #define EXPECT_GE(val1, val2) \
 
 #define EXPECT_GT(val1, val2) \
+
+#define EXPECT_END()          \
+    return 0;
 
 #endif //__MTEST_H__
